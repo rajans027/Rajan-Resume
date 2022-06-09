9132838b1542de4c180876a43cbc51f5b8b685be
rajans027 <43797466+rajans027@users.noreply.github.com>
2022-06-09 16:57:24 -0400

    Update test_handler.py

--- Cloud-resume/tests/unit/test_handler.py
+++ Cloud-resume/tests/unit/test_handler.py
@@ -1,73 +1,39 @@
+import os
+import re
 import json
+from unittest import mock
 
-import pytest
+from function import app
 
-from hello_world import app
+with open('Cloud-resume/template.yaml', 'r') as f:
+    TABLENAME = re.search(r'TableName: (.*)?', f.read()).group(1)
 
+@mock.patch.dict(os.environ, {"TABLENAME": TABLENAME})
+def test_lambda_handler():
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+    # check AWS credentials
+#     print(os.environ.keys())
+#     assert "AWS_ACCESS_KEY_ID" in os.environ
+#     assert "AWS_SECRET_ACCESS_KEY" in os.environ
 
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+    ret = app.lambda_handler("", "")
 
+    # assert return keys
+    assert "statusCode" in ret
+    assert "headers" in ret
+    assert "body" in ret
 
-def test_lambda_handler(apigw_event, mocker):
+    # check headers for CORS
+    assert "Access-Control-Allow-Origin" in ret["headers"]
+    assert "Access-Control-Allow-Methods" in ret["headers"]
+    assert "Access-Control-Allow-Headers" in ret["headers"]
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
+    # check status code
+    if ret["statusCode"] == 200:
+        # check body, count
+        assert "Count" in ret["body"]
+        assert json.loads(ret["body"])["Count"].isnumeric()
+    else:
+        assert json.loads(ret["body"])["Count"] == -1
 
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+    return
